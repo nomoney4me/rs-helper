ba6dc0cf4aa7c36e3433b6408df597194c7c46a9
TB <9965349+nomoney4me@users.noreply.github.com>
2021-02-06 17:22:58 -0600

    fixed !profile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const {prefix, token} = require('./config.json');
 
 const players = require('./lib/funcs/players');
 
@@ -8,22 +8,44 @@ client.once('ready', () => {
   console.log('ready')
 })
 
-client.login(config.token)
+client.login(token)
 
 
 client.on('message', message => {
-  var embed = {
-
-  }
-
-
   var content = message.content.split(' ');
-  console.log(content)
   var command = content.shift()
-  if(command === '!profile') {
+  if(command === `${prefix}profile`) {  
     players.getPlayerProfile(content[0], 5).then(res => {
-      console.log(res)
-      message.channel.send(JSON.stringify(res))
+      if(res.error) 
+      throw (`Player \`${content[0]}\` does not exist, check your spelling.`)
+
+      var eventsCombined = res.activities.map(row => {
+        return `[${row.date}] ${row.text}\n----- ${row.details}`
+      }, [])
+
+      var embed = {
+        "content": `\nUser: ${res.name}\nCombat Level: ${res.combatlevel}\nRank: ${res.rank}`,
+        "embed": 
+          {
+            "description": `Logged In: ${res.loggedIn}`,
+            "color": 9756011,
+            "fields": [
+              {
+                "name": "Melee / Ranged / Magic",
+                "value": `${res.melee} / ${res.ranged} / ${res.magic}`
+              },
+              {
+                "name": "Last 5 Events:",
+                "value": eventsCombined.join('\n\n')
+              }
+            ]
+          }
+        
+      }
+      
+      message.reply(embed)
+    }).catch(e => {
+      message.reply(e)
     })
     // message.channel.send(`fetching profile of: ${content.join(' ')}`)
   }
